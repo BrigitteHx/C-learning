33a655bf2084b0dd439636c58d8d249cfea9348c
BrigitteHx <brigitteheijkoop@gmail.com>
2024-03-27 20:05:27 +0100

    styling in laravel and to do list project done :)

--- Fase2/LaravelPractice/task-list/resources/views/index.blade.php
+++ Fase2/LaravelPractice/task-list/resources/views/index.blade.php
@@ -3,17 +3,17 @@
 
 @section('content')
   <nav class="mb-4">
-    <a href="{{route('tasks.create')}} " class="font-medium text-gray-700 underline decoration-pink">Add Task!</a>
-</nav>
+    <a href="{{ route('tasks.create') }}" class="link">Add Task!</a>
+  </nav>
 
   @forelse ($tasks as $task)
     <div>
-      <a href="{{ route('tasks.show', ['task' => $task->id]) }}" @class(['line-through' => $task->completed])>{{ $task->title }}</a>
+      <a href="{{ route('tasks.show', ['task' => $task->id]) }}"
+        @class(['line-through' => $task->completed])>{{ $task->title }}</a>
     </div>
   @empty
     <div>There are no tasks!</div>
   @endforelse
-
   @if ($tasks->count())
     <nav class="mt-4">
       {{ $tasks->links() }}
