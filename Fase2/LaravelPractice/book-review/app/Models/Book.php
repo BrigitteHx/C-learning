b01da45f9bc555b54e58b0cb4e56335e9e2f31f7
BrigitteHx <brigitteheijkoop@gmail.com>
2024-04-05 10:08:26 +0200

    popular/ highest rated logic and view book-review

--- Fase2/LaravelPractice/book-review/app/Models/Book.php
+++ Fase2/LaravelPractice/book-review/app/Models/Book.php
@@ -10,6 +10,9 @@ use Illuminate\Database\Query\Builder as QueryBuilder;
 class Book extends Model
 {
     use HasFactory;
+
+    // query scoper for tinker & used in filter:
+
     public function reviews()
     {
         return $this->hasMany(Review::class);
@@ -40,28 +43,59 @@ class Book extends Model
         return $query->having('reviews_count', '>=', $minReviews);
     }
 
+    // function doesnt work bc bad call: 
+    
+    // private function dateRangeFilter(Builder $query, $from = null, $to = null)
+    // {
+    //     return $query->withRecentReviews(fn($date) => $date->subWeek());
+    //     if ($from && !$to) {
+    //         $query->where('created_at', '>=', $from);
+    //     } elseif (!$from && $to) {
+    //         $query->where('created_at', '<=', $to);
+    //     } elseif ($from && $to) {
+    //         $query->whereBetween('created_at', [$from, $to]);
+    //     }
+    // }
+
     private function dateRangeFilter(Builder $query, $from = null, $to = null)
     {
-        return $query->withRecentReviews(fn($date) => $date->subWeek());
         if ($from && !$to) {
-            $query->where('created_at', '>=', $from);
+            $query->whereDate('created_at', '>=', $from);
         } elseif (!$from && $to) {
-            $query->where('created_at', '<=', $to);
+            $query->whereDate('created_at', '<=', $to);
         } elseif ($from && $to) {
             $query->whereBetween('created_at', [$from, $to]);
         }
+        return $query;
     }
 
-//     private function dateRangeFilter(Builder $query, $from = null, $to = null)
-//     {
-//         if ($from && !$to) {
-//             $query->whereDate('created_at', '>=', $from);
-//         } elseif (!$from && $to) {
-//             $query->whereDate('created_at', '<=', $to);
-//         } elseif ($from && $to) {
-//             $query->whereBetween('created_at', [$from, $to]);
-//         }
-//         return $query;
-//     }
+    // query scopes for popularity filter under search:
 
+    public function scopePopularLastMonth(Builder $query): Builder|QueryBuilder
+    {
+        return $query->popular(now()->subMonth(), now())
+            ->highestRated(now()->subMonth(), now())
+            ->minReviews(2);
+    }
+
+    public function scopePopularLast6Months(Builder $query): Builder|QueryBuilder
+    {
+        return $query->popular(now()->subMonths(6), now())
+            ->highestRated(now()->subMonths(6), now())
+            ->minReviews(5);
+    }
+
+    public function scopeHighestRatedLastMonth(Builder $query): Builder|QueryBuilder
+    {
+        return $query->highestRated(now()->subMonth(), now())
+            ->popular(now()->subMonth(), now())
+            ->minReviews(2);
+    }
+
+    public function scopeHighestRatedLast6Months(Builder $query): Builder|QueryBuilder
+    {
+        return $query->highestRated(now()->subMonths(6), now())
+            ->popular(now()->subMonths(6), now())
+            ->minReviews(5);
+    }
 }
