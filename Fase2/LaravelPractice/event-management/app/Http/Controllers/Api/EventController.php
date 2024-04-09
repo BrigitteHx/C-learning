a5a993f5a4c6f719325fc70beddcedb8338031d4
BrigitteHx <brigitteheijkoop@gmail.com>
2024-04-09 11:40:34 +0200

    api resources event-management

--- Fase2/LaravelPractice/event-management/app/Http/Controllers/Api/EventController.php
+++ Fase2/LaravelPractice/event-management/app/Http/Controllers/Api/EventController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
@@ -12,7 +13,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        return Event::all();
+        return EventResource::collection(Event::with('user')->get());
     }
 
     /**
@@ -38,7 +39,8 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        return $event;
+        $event->load('user', 'attendees');
+        return new EventResource($event);
     }
     /**
      * Update the specified resource in storage.
@@ -54,7 +56,7 @@ class EventController extends Controller
             ])
         );
 
-        return $event;
+        return new EventResource($event);
     }
     /**
      * Remove the specified resource from storage.
