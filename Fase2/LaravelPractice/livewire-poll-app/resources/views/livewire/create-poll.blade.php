919aa2ef5b449e18ad21e59dadff978b54876555
BrigitteHx <brigitteheijkoop@gmail.com>
2024-04-30 15:45:30 +0200

    validation in livewire, polls component & communication between components

--- Fase2/LaravelPractice/livewire-poll-app/resources/views/livewire/create-poll.blade.php
+++ Fase2/LaravelPractice/livewire-poll-app/resources/views/livewire/create-poll.blade.php
@@ -4,9 +4,14 @@
 
     <input type="text" wire:model.live="title" />
 
+    @error('title')
+      <div class="text-red-500">{{ $message }}</div>
+    @enderror
+
     <div class="mb-4 mt-4">
       <button class="btn" wire:click.prevent="addOption">Add Option</button>
     </div>
+
     <div>
       @foreach ($options as $index => $option)
         <div class="mb-4">
@@ -16,6 +21,9 @@
             <button class="btn"
               wire:click.prevent="removeOption({{ $index }})">Remove</button>
           </div>
+          @error("options.{$index}")
+            <div class="text-red-500">{{ $message }}</div>
+          @enderror
         </div>
       @endforeach
     </div>
