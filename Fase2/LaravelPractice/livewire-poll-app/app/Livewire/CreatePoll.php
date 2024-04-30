dedc8a7821fdb7ff1519ddb6889cdfeb12e54132
BrigitteHx <brigitteheijkoop@gmail.com>
2024-04-30 15:00:15 +0200

    first livewire, actions in livewire, editing options

--- Fase2/LaravelPractice/livewire-poll-app/app/Livewire/CreatePoll.php
+++ Fase2/LaravelPractice/livewire-poll-app/app/Livewire/CreatePoll.php
@@ -4,13 +4,27 @@ namespace App\Livewire;
 // check dit voor ieder volgend bestand
 
 use Livewire\Component;
-
 class CreatePoll extends Component
 {
     public $title;
-
+    public $options = ['First'];
     public function render()
     {
         return view('livewire.create-poll');
     }
+    public function addOption()
+    {
+        $this->options[] = '';
+    }
+
+    public function removeOption($index)
+    {
+        unset($this->options[$index]);
+        $this->options = array_values($this->options);
+    }
+
+// public function mount()
+// {
+
+// }
 }
