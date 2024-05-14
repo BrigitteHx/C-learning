c6bc8befd3605c823aca56d60cca53df41f7c418
BrigitteHx <brigitteheijkoop@gmail.com>
2024-05-14 09:39:26 +0200

    label component & flash errors

--- Fase2/LaravelPractice/job-board/resources/views/job_application/create.blade.php
+++ Fase2/LaravelPractice/job-board/resources/views/job_application/create.blade.php
@@ -18,12 +18,12 @@
       @csrf
       <div class="mb-4">
         <label for="expected_salary"
-          class="mb-2 block text-sm font-medium text-slate-900">Expected Salary</label>
+          class="mb-2 block text-sm font-medium text-slate-900">Expected Salary *</label>
         <x-text-input type="number" name="expected_salary" />
       </div>
 
       <div class="mb-4">
-        <label class="mb-2 block text-sm font-medium text-slate-900">Upload CV</label>
+        <label class="mb-2 block text-sm font-medium text-slate-900">Upload CV *</label>
         <x-text-input type="file" name="cv" />
       </div>
 
