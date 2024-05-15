4597c3a44ed589c573239810b19b525e86423407
BrigitteHx <brigitteheijkoop@gmail.com>
2024-05-15 15:04:18 +0200

    all employer functionality implemented: register, middleware, jobs form, editing logic, policy, soft deletes -> finally done :)

--- Fase2/LaravelPractice/job-board/app/Http/Controllers/MyJobApplicationController.php
+++ Fase2/LaravelPractice/job-board/app/Http/Controllers/MyJobApplicationController.php
@@ -1,10 +1,7 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Models\JobApplication;
 use Illuminate\Http\Request;
-
 class MyJobApplicationController extends Controller
 {
     public function index()
@@ -15,18 +12,17 @@ class MyJobApplicationController extends Controller
                 'applications' => auth()->user()->jobApplications()
                     ->with([
                         'job' => fn($query) => $query->withCount('jobApplications')
-                            ->withAvg('jobApplications', 'expected_salary'),
+                            ->withAvg('jobApplications', 'expected_salary')
+                            ->withTrashed(),
                         'job.employer'
                     ])
                     ->latest()->get()
             ]
         );
     }
-
     public function destroy(JobApplication $myJobApplication)
     {
         $myJobApplication->delete();
-
         return redirect()->back()->with(
             'success',
             'Job application removed.'
--- Fase2/LaravelPractice/job-board/app/Models/Job.php
+++ Fase2/LaravelPractice/job-board/app/Models/Job.php
@@ -1,18 +1,25 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Query\Builder as QueryBuilder;
-
 class Job extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'location',
+        'salary',
+        'description',
+        'experience',
+        'category'
+    ];
 
     public static array $experience = ['entry', 'intermediate', 'senior'];
     public static array $category = [
@@ -21,17 +28,14 @@ class Job extends Model
         'Sales',
         'Marketing'
     ];
-
     public function employer(): BelongsTo
     {
         return $this->belongsTo(Employer::class);
     }
-
     public function jobApplications(): HasMany
     {
         return $this->hasMany(JobApplication::class);
     }
-
     public function hasUserApplied(Authenticatable|User|int $user): bool
     {
         return $this->where('id', $this->id)
@@ -40,7 +44,6 @@ class Job extends Model
                 fn($query) => $query->where('user_id', '=', $user->id ?? $user)
             )->exists();
     }
-
     public function scopeFilter(Builder|QueryBuilder $query, array $filters): Builder|QueryBuilder
     {
         return $query->when($filters['search'] ?? null, function ($query, $search) {
